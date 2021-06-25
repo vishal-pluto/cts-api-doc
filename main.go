850c9858e56f21930cbb04fd00f50c73151fab4b
vishal-pluto <vishal@pluto.health>
2021-06-25 11:04:05 +0000

    new endpoints added & endpoints title changed to path from description

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
 
-	log.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+	log.Println("Listening on :8083...")
+	err := http.ListenAndServe(":8083", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
